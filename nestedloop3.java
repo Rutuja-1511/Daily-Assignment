96ad6c4026bc2b3344df2108d6b25c622299cae7
Rutuja Narawade <114046019+Rutuja-1511@users.noreply.github.com>
2022-09-29 08:57:25 +0530

    Add files via upload

---
+++ nestedloop3.java
@@ -0,0 +1,29 @@
+package Daily_Assignment;
+
+// Triangle Pattern
+
+import java.util.Scanner;
+
+public class nestedloop3 
+{
+
+	public static void main(String[] args) 
+{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the value of row");
+		int n=sc.nextInt(); 
+
+              for(int i=1;i<=n;i++)
+              {
+	          for(int j=1;j<=n-i;j++) 
+	          {
+		        System.out.print(" ");
+              }
+	          for(int j=1;j<=i;j++) 
+	          {
+		System.out.print("* ");
+	          }
+	System.out.println(" ");
+              }
+}
+}
